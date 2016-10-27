eed6a45f27edd9e63ed2bab69511c92cf942dc32
Derek <dereklee@downtothis.com>
2016-10-28 00:04:51 +0900

    Some tests for the customer statement method.

--- RefactoringBookSampleTests/Components/Customer/CustomerTest.swift
+++ RefactoringBookSampleTests/Components/Customer/CustomerTest.swift
@@ -22,4 +22,35 @@ class CustomerTest: XCTestCase {
         XCTAssertEqual(customer.rentals, expectedRentals)
     }
 
+    func testStatement_noRentals() {
+        let customer = Customer(name: "John Doe")
+
+
+        let actualStatement = customer.statement()
+
+
+        let expectedStatement =
+            "Rental Record for John Doe\n" +
+            "Amount owed is 0.0\n" +
+            "You earned 0 frequent renter points"
+        XCTAssertEqual(actualStatement, expectedStatement)
+    }
+
+    func testStatement_regular_oneDayRental() {
+        let customer = Customer(name: "John Doe")
+
+        let regularRental = RentalFixture.Regular(days: 1)
+        customer.addRental(regularRental)
+
+
+        let actualStatement = customer.statement()
+
+
+        let expectedStatement =
+            "Rental Record for John Doe\n" +
+            "\tDie Hard\t2.0\n" +
+            "Amount owed is 2.0\nYou earned 1 frequent renter points"
+        XCTAssertEqual(actualStatement, expectedStatement)
+    }
+
 }
