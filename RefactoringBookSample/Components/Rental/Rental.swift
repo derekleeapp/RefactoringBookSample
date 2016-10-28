fdc13fa74b527b6050f6f383be8d59c4714127ec
derekleeapp <dereklee@Naomis-MacBook.local>
2016-10-28 15:16:39 +0900

    Step 1: Move getAmount() to Rental as getCharge()

--- RefactoringBookSample/Components/Rental/Rental.swift
+++ RefactoringBookSample/Components/Rental/Rental.swift
@@ -6,6 +6,35 @@ class Rental {
         self.movie = movie
         self.daysRented = daysRented
     }
+
+    func getCharge() -> Double {
+        var result: Double = 0.0
+
+        switch (movie.priceCode) {
+
+        case Movie.Regular:
+            result += 2
+            if (daysRented > 2) {
+                result += Double(daysRented - 2) * 1.5
+            }
+
+        case Movie.NewRelease:
+            result += Double(daysRented) * 3
+
+        case Movie.Childrens:
+            result += 1.5
+            if (daysRented > 3) {
+                result += Double(daysRented - 3) * 1.5
+            }
+
+        default:
+            break
+
+        }
+
+        return result
+    }
+
 }
 
 extension Rental: Equatable {}
