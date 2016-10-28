039b4f01c377881c33f8565bfe3846caa4f86794
derekleeapp <dereklee@Naomis-MacBook.local>
2016-10-28 14:21:24 +0900

    Add Carthage; restructure tests to use Quick.

--- RefactoringBookSampleTests/Components/Customer/CustomerTest.swift
+++ RefactoringBookSampleTests/Components/Customer/CustomerTest.swift
@@ -1,125 +1,138 @@
-import XCTest
+import Quick
+import Nimble
 @testable import RefactoringBookSample
 
-class CustomerTest: XCTestCase {
+class CustomerTest: QuickSpec {
 
-    var customer: Customer!
+    override func spec() {
+        var customer: Customer!
 
-    override func setUp() {
-        customer = Customer(name: "John Doe")
-    }
+        beforeEach {
+            customer = Customer(name: "John Doe")
+        }
 
-    func testCustomer_initializesWithoutRentals() {
-        XCTAssertEqual(customer.rentals, [])
-    }
+        it("initializes a new customer without any rentals") {
+            expect(customer.rentals).to(equal([]))
+        }
 
-    func testAddRental_addsARentalToTheList() {
-        let newReleaseRental = RentalFixture.NewRelease(days: 1)
-        customer.addRental(newReleaseRental)
-
-
-        let expectedRentals = [newReleaseRental]
-        XCTAssertEqual(customer.rentals, expectedRentals)
-    }
-
-    func testStatement_noRentals() {
-        let actualStatement = customer.statement()
+        it("adds a rental to the list of rentals") {
+            let newReleaseRental = RentalFixture.NewRelease(days: 1)
+            customer.addRental(newReleaseRental)
 
 
-        let expectedStatement =
-            "Rental Record for John Doe\n" +
-            "Amount owed is 0.0\n" +
-            "You earned 0 frequent renter points"
-        XCTAssertEqual(actualStatement, expectedStatement)
-    }
+            let expectedRentals = [newReleaseRental]
+            expect(customer.rentals).to(equal(expectedRentals))
+        }
 
-    func testStatement_regular_oneDayRental() {
-        let regularRental = RentalFixture.Regular(days: 1)
-        customer.addRental(regularRental)
+        describe("the customer statement") {
+
+            it("indicates when there have been no rentals") {
+                let actualStatement = customer.statement()
 
 
-        let actualStatement = customer.statement()
+                let expectedStatement =
+                    "Rental Record for John Doe\n" +
+                    "Amount owed is 0.0\n" +
+                    "You earned 0 frequent renter points"
+                expect(actualStatement).to(equal(expectedStatement))
+            }
+
+            describe("regular rentals") {
+                it("reflects charges and points when rented for one day") {
+                    let regularRental = RentalFixture.Regular(days: 1)
+                    customer.addRental(regularRental)
 
 
-        let expectedStatement =
-            "Rental Record for John Doe\n" +
-            "\tDie Hard\t2.0\n" +
-            "Amount owed is 2.0\nYou earned 1 frequent renter points"
-        XCTAssertEqual(actualStatement, expectedStatement)
-    }
-
-    func testStatement_regular_multiDayRental() {
-        let regularRental = RentalFixture.Regular(days: 3)
-        customer.addRental(regularRental)
+                    let actualStatement = customer.statement()
 
 
-        let actualStatement = customer.statement()
+                    let expectedStatement =
+                        "Rental Record for John Doe\n" +
+                        "\tDie Hard\t2.0\n" +
+                        "Amount owed is 2.0\nYou earned 1 frequent renter points"
+                    expect(actualStatement).to(equal(expectedStatement))
+                }
+
+                it("reflects charges and points when rented for multiple days") {
+                    let regularRental = RentalFixture.Regular(days: 3)
+                    customer.addRental(regularRental)
 
 
-        let expectedStatement =
-            "Rental Record for John Doe\n" +
-            "\tDie Hard\t3.5\n" +
-            "Amount owed is 3.5\nYou earned 1 frequent renter points"
-        XCTAssertEqual(actualStatement, expectedStatement)
-    }
-
-    func testStatement_newRelease_oneDayRental() {
-        let newReleaseRental = RentalFixture.NewRelease(days: 1)
-        customer.addRental(newReleaseRental)
+                    let actualStatement = customer.statement()
 
 
-        let actualStatement = customer.statement()
+                    let expectedStatement =
+                        "Rental Record for John Doe\n" +
+                        "\tDie Hard\t3.5\n" +
+                        "Amount owed is 3.5\nYou earned 1 frequent renter points"
+                    expect(actualStatement).to(equal(expectedStatement))
+                }
+            }
+
+            describe("new release rentals") {
+                it("reflects charges and points when rented for one day") {
+                    let newReleaseRental = RentalFixture.NewRelease(days: 1)
+                    customer.addRental(newReleaseRental)
 
 
-        let expectedStatement =
-            "Rental Record for John Doe\n" +
-            "\tHunger Games\t3.0\n" +
-            "Amount owed is 3.0\nYou earned 1 frequent renter points"
-        XCTAssertEqual(actualStatement, expectedStatement)
-    }
-
-    func testStatement_newRelease_multiDayRental() {
-        let newReleaseRental = RentalFixture.NewRelease(days: 2)
-        customer.addRental(newReleaseRental)
+                    let actualStatement = customer.statement()
 
 
-        let actualStatement = customer.statement()
+                    let expectedStatement =
+                        "Rental Record for John Doe\n" +
+                            "\tHunger Games\t3.0\n" +
+                    "Amount owed is 3.0\nYou earned 1 frequent renter points"
+                    expect(actualStatement).to(equal(expectedStatement))
+                }
+
+                it("reflects charges and points when rented for multiple days") {
+                    let newReleaseRental = RentalFixture.NewRelease(days: 2)
+                    customer.addRental(newReleaseRental)
 
 
-        let expectedStatement =
-            "Rental Record for John Doe\n" +
-                "\tHunger Games\t6.0\n" +
-        "Amount owed is 6.0\nYou earned 2 frequent renter points"
-        XCTAssertEqual(actualStatement, expectedStatement)
-    }
-
-    func testStatement_childrens_oneDayRental() {
-        let childrensRental = RentalFixture.Childrens(days: 1)
-        customer.addRental(childrensRental)
+                    let actualStatement = customer.statement()
 
 
-        let actualStatement = customer.statement()
+                    let expectedStatement =
+                        "Rental Record for John Doe\n" +
+                            "\tHunger Games\t6.0\n" +
+                    "Amount owed is 6.0\nYou earned 2 frequent renter points"
+                    XCTAssertEqual(actualStatement, expectedStatement)
+                    expect(actualStatement).to(equal(expectedStatement))
+                }
+            }
+
+            describe("childrens rentals") {
+                it("reflects charges and points when rented for one day") {
+                    let childrensRental = RentalFixture.Childrens(days: 1)
+                    customer.addRental(childrensRental)
 
 
-        let expectedStatement =
-            "Rental Record for John Doe\n" +
-            "\tMinions\t1.5\n" +
-            "Amount owed is 1.5\nYou earned 1 frequent renter points"
-        XCTAssertEqual(actualStatement, expectedStatement)
-    }
-
-    func testStatement_childrens_multiDayRental() {
-        let childrensRental = RentalFixture.Childrens(days: 4)
-        customer.addRental(childrensRental)
+                    let actualStatement = customer.statement()
 
 
-        let actualStatement = customer.statement()
+                    let expectedStatement =
+                        "Rental Record for John Doe\n" +
+                            "\tMinions\t1.5\n" +
+                    "Amount owed is 1.5\nYou earned 1 frequent renter points"
+                    expect(actualStatement).to(equal(expectedStatement))
+                }
+
+                it("reflects charges and points when rented for multiple days") {
+                    let childrensRental = RentalFixture.Childrens(days: 4)
+                    customer.addRental(childrensRental)
 
 
-        let expectedStatement =
-            "Rental Record for John Doe\n" +
-            "\tMinions\t3.0\n" +
-            "Amount owed is 3.0\nYou earned 1 frequent renter points"
-        XCTAssertEqual(actualStatement, expectedStatement)
+                    let actualStatement = customer.statement()
+
+
+                    let expectedStatement =
+                        "Rental Record for John Doe\n" +
+                            "\tMinions\t3.0\n" +
+                    "Amount owed is 3.0\nYou earned 1 frequent renter points"
+                    expect(actualStatement).to(equal(expectedStatement))
+                }
+            }
+        }
     }
 }
