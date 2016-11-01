ab5b583a5d50057faf40ad6731d312b23f7b4991
derekleeapp <dereklee@downtothis.com>
2016-11-01 18:54:00 +0900

    Step 16: Remove base class implementation of getCharge()
    The book recommends declaring the function as abstract however this is a
    bit different in Swift. Wondering if there is a good way to do this
    without declaring multiple protocols.

--- RefactoringBookSample/Components/Movie/Price.swift
+++ RefactoringBookSample/Components/Movie/Price.swift
@@ -4,30 +4,6 @@ class Price {
     }
 
     func getCharge(daysRented: Int) -> Double {
-        var result: Double = 0.0
-
-        switch (getPriceCode()) {
-
-        case Movie.Regular:
-            result += 2
-            if (daysRented > 2) {
-                result += Double(daysRented - 2) * 1.5
-            }
-
-        case Movie.NewRelease:
-            result += Double(daysRented) * 3
-
-        case Movie.Childrens:
-            result += 1.5
-            if (daysRented > 3) {
-                result += Double(daysRented - 3) * 1.5
-            }
-
-        default:
-            break
-
-        }
-
-        return result
+        return -1
     }
 }
