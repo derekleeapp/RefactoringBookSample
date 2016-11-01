af7420694f842cc689426813d21db1a116ebf8c9
derekleeapp <dereklee@downtothis.com>
2016-11-01 18:50:56 +0900

    Step 14: Replace conditional w/polymorphism: New Release

--- RefactoringBookSample/Components/Movie/NewReleasePrice.swift
+++ RefactoringBookSample/Components/Movie/NewReleasePrice.swift
@@ -2,4 +2,8 @@ class NewReleasePrice: Price {
     override func getPriceCode() -> Int {
         return Movie.NewRelease
     }
+
+    override func getCharge(daysRented: Int) -> Double {
+        return Double(daysRented) * 3
+    }
 }
