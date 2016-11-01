df3e20509bc0dbccaba56ef0ccd00830940e3cb8
derekleeapp <dereklee@downtothis.com>
2016-11-01 18:52:42 +0900

    Step 15: Replace conditional w/polymorphism: Childrens

--- RefactoringBookSample/Components/Movie/ChildrensPrice.swift
+++ RefactoringBookSample/Components/Movie/ChildrensPrice.swift
@@ -2,4 +2,14 @@ class ChildrensPrice: Price {
     override func getPriceCode() -> Int {
         return Movie.Childrens
     }
+
+    override func getCharge(daysRented: Int) -> Double {
+        var result: Double = 1.5
+
+        if (daysRented > 3) {
+            result += Double(daysRented - 3) * 1.5
+        }
+
+        return result
+    }
 }
