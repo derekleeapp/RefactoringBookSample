59ab9f1932a5cd48fe54037c5e94dfb94fad9671
derekleeapp <dereklee@downtothis.com>
2016-11-01 18:47:45 +0900

    Step 13: Replace conditional w/polymorphism: Regular
    Note that given that this is an override the original function is not
    called for this case.
    
    Changing the implementation confirms this with the tests.

--- RefactoringBookSample/Components/Movie/RegularPrice.swift
+++ RefactoringBookSample/Components/Movie/RegularPrice.swift
@@ -2,4 +2,14 @@ class RegularPrice: Price {
     override func getPriceCode() -> Int {
         return Movie.Regular
     }
+
+    override func getCharge(daysRented: Int) -> Double {
+        var result: Double = 2.0
+
+        if (daysRented > 2) {
+            result += Double(daysRented - 2) * 1.5
+        }
+
+        return result
+    }
 }
